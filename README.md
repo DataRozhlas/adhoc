ba6db11860e449ea2ac1e0cabee8d503c6bf6d98
Petr Kočí <petr.koci@samizdat.cz>
2024-05-24 16:03:03 +0200

    boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+Grafy pro iROZHLAS.cz
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-xl underline">
+        Vole
+      </div>
+
+    </>
+  )
+}
+
+export default App
